86d9a81b3e46d8addfd8743db7463975ef9dc482
Ryo Ota <nwtgck@nwtgck.org>
2020-09-01 02:21:38 +0900

    not use Box and dyn Trait

--- src/req_res_handler.rs
+++ src/req_res_handler.rs
@@ -3,21 +3,22 @@ use futures::FutureExt;
 use hyper::{Body, Request, Response};
 use std::future::Future;
 
-// NOTE: futures::future::Then<..., oneshot::Receiver, ...> can be a Future
+// NOTE: futures::future::Map<..., oneshot::Receiver, ...> can be a Future
 pub fn req_res_handler<Fut>(
     mut handler: impl FnMut(Request<Body>, oneshot::Sender<Response<Body>>) -> Fut,
 ) -> impl (FnMut(
     Request<Body>,
-) -> futures::future::Then<
-    Fut,
-    oneshot::Receiver<Response<Body>>,
-    Box<dyn FnOnce(()) -> oneshot::Receiver<Response<Body>> + Send>,
+) -> futures::future::Map<
+    futures::future::Join<Fut, oneshot::Receiver<Response<Body>>>,
+    fn(
+        ((), Result<Response<Body>, oneshot::Canceled>),
+    ) -> Result<Response<Body>, oneshot::Canceled>,
 >)
 where
     Fut: Future<Output = ()>,
 {
     move |req| {
         let (res_sender, res_receiver) = oneshot::channel::<Response<Body>>();
-        handler(req, res_sender).then(Box::new(move |_| res_receiver))
+        futures::future::join(handler(req, res_sender), res_receiver).map(|(_, x)| x)
     }
 }
