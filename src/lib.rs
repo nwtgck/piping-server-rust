817c9d31714fb875e409a8f0de4a1b462cb55038
Ryo Ota <nwtgck@nwtgck.org>
2024-01-17 16:35:04 +0900

    use hyper 1.x

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-pub mod dynamic_resources;
+mod dynamic_resources;
 mod macros;
 pub mod piping_server;
 pub mod util;
--- src/util.rs
+++ src/util.rs
@@ -1,14 +1,14 @@
+use core::convert::Infallible;
 use core::convert::TryFrom;
+use core::future::Future;
+use core::ops::Deref as _;
 use core::pin::Pin;
 use core::task::{Context, Poll};
-use futures::channel::oneshot;
-use futures::ready;
+use http_body_util::BodyExt as _;
+use hyper::body::Bytes;
 use pin_project_lite::pin_project;
 use std::collections::HashMap;
-use std::ops::Deref;
-use std::sync::{Arc, RwLock};
-use tokio::net::TcpStream;
-use tokio_rustls::server::TlsStream;
+use std::sync::Arc;
 
 pub trait OptionHeaderBuilder {
     // Add optional header
@@ -64,19 +64,23 @@ impl HeaderValuesBuilder for http::response::Builder {
 }
 
 pin_project! {
-    pub struct FinishDetectableStream<S> {
+    pub struct FinishDetectableBody<B> {
         #[pin]
-        stream_pin: S,
-        finish_notifier: Option<oneshot::Sender<()>>,
+        body: B,
+        finish_notifier: Option<futures::channel::oneshot::Sender<()>>,
     }
 }
 
-impl<S: futures::stream::Stream> futures::stream::Stream for FinishDetectableStream<S> {
-    type Item = S::Item;
+impl<B: http_body::Body> http_body::Body for FinishDetectableBody<B> {
+    type Data = B::Data;
+    type Error = B::Error;
 
-    fn poll_next(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
+    fn poll_frame(
+        self: Pin<&mut Self>,
+        cx: &mut Context<'_>,
+    ) -> Poll<Option<Result<http_body::Frame<Self::Data>, Self::Error>>> {
         let mut this = self.project();
-        match this.stream_pin.as_mut().poll_next(cx) {
+        match this.body.as_mut().poll_frame(cx) {
             // If body is finished
             Poll::Ready(None) => {
                 // Notify finish
@@ -88,79 +92,61 @@ impl<S: futures::stream::Stream> futures::stream::Stream for FinishDetectableStr
             poll => poll,
         }
     }
+
+    #[inline]
+    fn is_end_stream(&self) -> bool {
+        self.body.is_end_stream()
+    }
+
+    #[inline]
+    fn size_hint(&self) -> http_body::SizeHint {
+        self.body.size_hint()
+    }
 }
 
-pub fn finish_detectable_stream<S>(
-    stream: S,
-) -> (FinishDetectableStream<S>, oneshot::Receiver<()>) {
-    let (finish_notifier, finish_waiter) = oneshot::channel::<()>();
+pub fn finish_detectable_body<B: http_body::Body>(
+    body: B,
+) -> (
+    FinishDetectableBody<B>,
+    futures::channel::oneshot::Receiver<()>,
+) {
+    let (finish_notifier, finish_waiter) = futures::channel::oneshot::channel::<()>();
     (
-        FinishDetectableStream {
-            stream_pin: stream,
+        FinishDetectableBody {
+            body,
             finish_notifier: Some(finish_notifier),
         },
         finish_waiter,
     )
 }
 
-pub fn make_io_error(err: String) -> std::io::Error {
-    std::io::Error::new(std::io::ErrorKind::Other, err)
-}
-
-fn read_private_keys(rd: &mut dyn std::io::BufRead) -> Result<Vec<Vec<u8>>, std::io::Error> {
-    let mut keys = Vec::<Vec<u8>>::new();
-
-    loop {
-        match rustls_pemfile::read_one(rd)? {
-            None => return Ok(keys),
-            Some(
-                rustls_pemfile::Item::RSAKey(key)
-                | rustls_pemfile::Item::PKCS8Key(key)
-                | rustls_pemfile::Item::ECKey(key),
-            ) => keys.push(key),
-            _ => {}
-        };
-    }
-}
-
-// (base: https://github.com/ctz/hyper-rustls/blob/5f073724f7b5eee3a2d72f0a86094fc2718b51cd/examples/server.rs)
 pub fn load_tls_config(
     cert_path: impl AsRef<std::path::Path>,
     key_path: impl AsRef<std::path::Path> + std::fmt::Display,
-) -> std::io::Result<rustls::ServerConfig> {
-    // Load public certificate.
-    let mut cert_reader = std::io::BufReader::new(std::fs::File::open(cert_path)?);
-    let certs = rustls_pemfile::certs(&mut cert_reader)
-        .map_err(|_| make_io_error("unable to load certificate".to_owned()))?;
-    // Load private key.
-    let mut key_reader = std::io::BufReader::new(std::fs::File::open(key_path)?);
-    // Load and return a single private key.
-    let mut private_keys = read_private_keys(&mut key_reader)
-        .map_err(|_| make_io_error("unable to load private key".to_owned()))?;
-    let key = if private_keys.len() > 0 {
-        Ok(private_keys.remove(0))
-    } else {
-        Err(make_io_error("failed to get private key".to_owned()))
-    }?;
-    let certificates: Vec<rustls::Certificate> =
-        certs.into_iter().map(rustls::Certificate).collect();
-    let mut cfg = rustls::ServerConfig::builder()
-        .with_safe_defaults()
+) -> anyhow::Result<rustls::ServerConfig> {
+    let certs = rustls_pemfile::certs(&mut std::io::BufReader::new(&mut std::fs::File::open(
+        cert_path,
+    )?))
+    .collect::<Result<Vec<_>, _>>()?;
+    let private_key = rustls_pemfile::private_key(&mut std::io::BufReader::new(
+        &mut std::fs::File::open(key_path)?,
+    ))?
+    .expect("private key not found");
+    let mut config = rustls::ServerConfig::builder()
         .with_no_client_auth()
-        .with_single_cert(certificates, rustls::PrivateKey(key))
-        .map_err(|_| make_io_error("failed to create ServerConfig".to_owned()))?;
-    // Configure ALPN to accept HTTP/2, HTTP/1.1 in that order.
-    cfg.alpn_protocols = vec![b"h2".to_vec(), b"http/1.1".to_vec()];
-    Ok(cfg)
+        .with_single_cert(certs, private_key)?;
+    config.alpn_protocols = vec![b"h2".to_vec(), b"http/1.1".to_vec()];
+    Ok(config)
 }
 
 pub fn hot_reload_tls_cfg(
+    tokio_handle: tokio::runtime::Handle,
     cert_path: impl AsRef<std::path::Path> + Send + Sync + 'static,
     key_path: impl AsRef<std::path::Path> + Send + Sync + std::fmt::Display + 'static,
-) -> Arc<RwLock<Arc<rustls::ServerConfig>>> {
+) -> Arc<tokio::sync::RwLock<Arc<rustls::ServerConfig>>> {
     let cert_path = Arc::new(cert_path);
     let key_path = Arc::new(key_path);
-    let tls_cfg_rwlock_arc = Arc::new(RwLock::new(Arc::new(
+    let tls_cfg_rwlock_arc = Arc::new(tokio::sync::RwLock::new(Arc::new(
         load_tls_config(cert_path.deref(), key_path.deref()).unwrap(),
     )));
 
@@ -189,8 +175,11 @@ pub fn hot_reload_tls_cfg(
                 match rx.recv() {
                     Ok(_event) => match load_tls_config(cert_path.deref(), key_path.deref()) {
                         Ok(tls_cfg) => {
-                            *(tls_cfg_rwlock.clone().write().unwrap()) = Arc::new(tls_cfg);
-                            log::info!("Successfully new certificates loaded");
+                            let tls_cfg_rwlock = tls_cfg_rwlock.clone();
+                            tokio_handle.spawn(async move {
+                                *(tls_cfg_rwlock.write().await) = Arc::new(tls_cfg);
+                                log::info!("Successfully new certificates loaded");
+                            });
                         }
                         Err(e) => log::error!("Failed to load new certificates: {e:?}"),
                     },
@@ -203,57 +192,21 @@ pub fn hot_reload_tls_cfg(
     return tls_cfg_rwlock_arc;
 }
 
-pin_project! {
-    pub struct HyperAcceptor<S> {
-        #[pin]
-        pub acceptor: S,
-    }
-}
-
-impl<S> hyper::server::accept::Accept for HyperAcceptor<S>
-where
-    S: futures::stream::Stream<Item = Result<TlsStream<TcpStream>, std::io::Error>>,
-{
-    type Conn = TlsStream<TcpStream>;
-    type Error = std::io::Error;
-
-    fn poll_accept(
-        self: core::pin::Pin<&mut Self>,
-        cx: &mut core::task::Context,
-    ) -> core::task::Poll<Option<Result<Self::Conn, Self::Error>>> {
-        self.project().acceptor.as_mut().poll_next(cx)
-    }
-}
-
-// (base: https://github.com/tokio-rs/tokio/blob/tokio-0.2.22/tokio/src/net/tcp/incoming.rs)
-/// Stream returned by the `TcpListener::incoming` function representing the
-/// stream of sockets received from a listener.
-#[must_use = "streams do nothing unless polled"]
-#[derive(Debug)]
-pub struct TokioIncoming<'a> {
-    inner: &'a mut tokio::net::TcpListener,
-}
-
-impl TokioIncoming<'_> {
-    pub fn new(listener: &mut tokio::net::TcpListener) -> TokioIncoming<'_> {
-        TokioIncoming { inner: listener }
-    }
-}
-
-impl futures::stream::Stream for TokioIncoming<'_> {
-    type Item = tokio::io::Result<TcpStream>;
-
-    #[allow(unused_mut)]
-    fn poll_next(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
-        let (socket, _) = ready!(self.inner.poll_accept(cx))?;
-        Poll::Ready(Some(Ok(socket)))
-    }
-}
-
 pub fn query_param_to_hash_map(query: Option<&str>) -> HashMap<String, String> {
     return match query {
         Some(query) => serde_urlencoded::from_str::<HashMap<String, String>>(query)
             .unwrap_or_else(|_| HashMap::new()),
         None => HashMap::new(),
     };
+}
+
+#[inline]
+pub fn full_body<E>(data: Bytes) -> impl http_body::Body<Data = Bytes, Error = E> {
+    http_body_util::Full::new(data)
+        .map_err(|_: Infallible| unreachable!("Error of Full::new() should be Infallible"))
+}
+
+#[inline]
+pub fn future_with_output_type<O, F: Future<Output = O>>(fut: F) -> F {
+    fut
 }
